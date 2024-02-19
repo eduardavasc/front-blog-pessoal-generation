a47f29b19c04d29deb393f18c6cb1987a1abc7f7
Maria Eduarda Vasconcelos <eduuardavasconcelos@gmail.com>
2024-02-19 17:30:43 -0300

    Funcionalidade Cadastrar Usuário e Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
+import React from 'react';
+import './App.css';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import { AuthProvider } from './contexts/AuthContext';
 import Navbar from './components/navBar/NavBar';
 import Home from './pages/home/Home';
-import Footer from './components/footer/Footer';
-import Login from './pages/login/Login';
 
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Navbar/>
-    <div className='min-h-[80vh]'>
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/home" element={<Home />} />   
-    </Routes>
-    </div>
-    <Footer/>
-    </BrowserRouter>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+        </AuthProvider>
     </>
-  )
+  );
 }
-
 export default App;
